e58771d96ad233024bbf4f1ca7615bc11a0a2860
nmh97 <hoangnguyen210297@gmail.com>
2021-01-22 21:56:37 +0700

    update final

--- ui.py
+++ ui.py
@@ -1,3 +1,5 @@
+
+
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
@@ -6,35 +8,42 @@ import PIL
 from tkinter import *
 from tkinter import messagebox
 import os
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+import os
 
-width = 600
-height = 400
-center = height // 2
+import cv2
+
+
+
+
+
+width = 300
+height = 300
+center = height//1
 white = (255, 255, 255)
 green = (0, 128, 0)
 
 
+
 def paint(event):
     # python_green = "#476042"
-    x1, y1 = (event.x - 2), (event.y - 2)
-    x2, y2 = (event.x + 2), (event.y + 2)
-    cv.create_oval(x1, y1, x2, y2, fill="black", width=5)
-    draw.line([x1, y1, x2, y2], fill="black", width=5)
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+    cv.create_oval(x1, y1, x2, y2, fill="black", width=15)
+    draw.line([x1, y1, x2, y2], fill="black", width=15)
+    #filename = "capture.bmp"
+    #image1.save(filename)
 
 
-def delete():
-    cv.delete(ALL)
-    if os.path.exists("capture.png"):
-        os.remove("capture.png")
-    #    messagebox.showwarning("Cảnh báo", "Đã xoá")
-    # else:
-    #    messagebox.showerror("Lỗi", "File không tồn tại")
 
 
-# load and prepare the image
 def load_image(filename):
+    # filename = "capture.png"
+    # image1.save(filename)
     # load the image
-    img = load_img("capture.png", grayscale=True, target_size=(28, 28))
+    img = load_img('capture.png', grayscale=True, target_size=(28, 28))
     # convert to array
     img = img_to_array(img)
     # reshape into a single sample with 1 channel
@@ -48,10 +57,10 @@ def load_image(filename):
 # load an image and predict the class
 def result():
     # save the image
-    # filename = "capture.png"
-    image1.save("capture.png")
+    filename = "capture.png"
+    image1.save(filename)
     # load the image
-    img = load_image("capture.png")
+    img = load_image('capture.png')
     # load model
     model = load_model('ai_model.h5')
     # predict the class
@@ -59,12 +68,73 @@ def result():
     messagebox.showinfo("Kết quả", digit[0])
 
 
-# entry point, run the example
+
+
+def delete():
+    cv.delete("all")
+    # if os.path.exists("capture.bmp"):
+    #     os.remove("capture.bmp")
+    #    messagebox.showwarning("Cảnh báo", "Đã xoá")
+    #else:
+    #    messagebox.showerror("Lỗi", "File không tồn tại")
+    root.update()
+
+
+    #filename = "capture.png"
+    #ilename = "capture1.png"
+    #image1.save(filename)
+    #img = load_image('cv')
+
+    #img = image1.os.path.exists("cature.png")
+    #img.save(img)
+
+
+    #image1.save(paint())
+    #image1.load(cv)
+    # load the image
+
+
+def openfn():
+    filename = filedialog.askopenfilename(title='open')
+
+    return filename
+
+
+def open_img():
+
+    x = openfn()
+    cv2 = Image.open(x)
+    cv2 = cv2.resize((50, 50), Image.ANTIALIAS)
+    cv2 = ImageTk.PhotoImage(cv2)
+    panel = Label(root, image=cv2)
+
+
+
+
+
+    panel.image = cv2
+    panel.pack()
+    img = load_image('cv2')
+    # load model
+    model = load_model('ai_model.h5')
+    # predict the class
+    digit = model.predict_classes(img)
+    messagebox.showinfo("Kết quả", digit[0])
+
+
+
+
+# load and prepare the image
+
+
+#entry point, run the example
 root = Tk()
 root.title('Nhận diện chữ số viết tay')
 
 # Tkinter create a canvas to draw on
 cv = Canvas(root, width=width, height=height, bg='white')
+cv.grid(row=0, column=0)
+cv.pack()
 
 # PIL create an empty image and draw object to draw on
 # memory only, not visible
@@ -83,8 +153,11 @@ cv.bind("<B1-Motion>", paint)
 # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
 # filename = "my_drawing.png"
 # image1.save(filename)
-btn_result = Button(root, text="Kết quả", command=result)
-btn_result.pack()
-btn_del = Button(root, text="Xoá", command=delete)
-btn_del.pack()
+
+button1 = Button(root, text="Xoá", command=delete)
+button1.pack()
+button = Button(root, text="Kết quả", command=result)
+button.pack()
+btn = Button(root, text='open image', command=open_img).pack()
 root.mainloop()
+
